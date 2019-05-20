88e5fa3be59d16da75c0d86bbba0031c782528f5
Soumyajit Pal <30958635+TutorSP@users.noreply.github.com>
2019-05-20 13:12:28 +0530

    Largest and Smallest from an Array
    WAP in Python to accept 10 numbers from user. Print the largest and smallest integers.

---
+++ largest_and_smallest.py
@@ -0,0 +1,5 @@
+my_list = []
+for i in range(10):
+	my_list.append(int(input("Enter a number: ")))
+print("The largest integer entered is {}".format(max(my_list)))
+print("The smallest integer entered is {}".format(min(my_list)))

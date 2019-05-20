b2c1e51964d157ba3376239cda96d3977ec39438
Soumyajit Pal <30958635+TutorSP@users.noreply.github.com>
2019-05-20 12:33:54 +0530

    Odd or Even
    WAP in Python to check and print whether a number is odd or even

---
+++ odd_even.py
@@ -0,0 +1,6 @@
+# WAP in Python to check whether a number is odd or even 
+number = int(input("Enter a positive integer: "))
+if number % 2 == 0:
+	print("{} is an even number".format(number))
+else:
+	print("{} is an odd number".format(number))
